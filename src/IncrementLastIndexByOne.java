49f99d6559bddee6a8f8b7395438228872d20f6d
DSK064 <saichowdary064@gmail.com>
2022-09-23 14:41:37 +0530

    first commit

--- src/IncrementLastIndexByOne.java
+++ src/IncrementLastIndexByOne.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class IncrementLaastIndexByOne {
+public class IncrementLastIndexByOne {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size:");
         int size = sc.nextInt();
         int[] arr = new int[size];
-        System.out.println("Entet the largest integer in the form of arra:");
+        System.out.println("Enter the largest integer in the form of array:");
         for(int i = 0; i < size; i++)
         {
             arr[i] = sc.nextInt();
